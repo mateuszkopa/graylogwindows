79ac5bab490f530093affe261754b6f939573baf
Mateusz Kopaszewski <m.kopaszewski@gmail.com>
2025-06-18 12:42:57 +0200

    s

--- graylog.sh
+++ graylog.sh
@@ -4,7 +4,7 @@
 sudo timedatectl set-timezone Europe/Warsaw
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install docker-compose nano apache2
+sudo apt-get install docker-compose nano apache2 python
 sudo usermod -aG docker $USER
 
 # Utworzenie katalogu graylog
@@ -16,6 +16,23 @@ echo "Utworzono katalog /graylog z uprawnieniami 777."
 cd /graylog
 sudo wget https://raw.githubusercontent.com/mateuszkopa/graylogwindows/refs/heads/main/docker-compose.yml 
 nano docker-compose.yml
+
+# Stworzenie agentów
+mkdir exe
+cd exe
+pip install pyinstaller
+wget https://raw.githubusercontent.com/mateuszkopa/graylogwindows/refs/heads/main/main.py
+chmod 777 main.py
+python main.py
+chmod 777 koncowki.py
+chmod 777 serwery.py
+pyinstaller --onefile "koncowki.py"
+pyinstaller --onefile "serwery.py"
+touch License.rtf
+mkdir staging staging/resources
+cp koncowki.exe staging
+cp serwery.exe staging
+cd staging/resources/
 
 # Przejcie do folderu apache
 chmod 777 /var/www/html/
